bda8a12139755548842be3b32c3a740246a53fcf
Fahim <fahimshahariar.fs@gmail.com>
2026-08-16 10:56:27 +0600

    Display and Utility Methods

--- main.py
+++ main.py
@@ -24,4 +24,36 @@ class ShoppingCart:
             return self.calculate_total() - discount
         else:
             return self.calculate_total()
-        
+    
+    def display_cart(self):
+        print(f"Customer Name: {self.customer_name}")
+        print("Items in Cart:")
+        for item in self.items:
+            print(f"- {item}")
+        print(f"Total Price: {self.calculate_total()}")
+        print(f"Price after Discount: {self.apply_discount()}")
+    
+    def clear_cart(self):
+        self.items = []
+        self.price = []
+        print("Cart has been cleared.")
+    
+    def __add__(self, other):
+        if isinstance(other, ShoppingCart):
+            combined_items = self.items + other.items
+            combined_price = self.price + other.price
+            return ShoppingCart(self.customer_name, combined_items, combined_price)
+        else:
+            raise ValueError("Can only add another ShoppingCart instance.")
+    
+
+cart1 = ShoppingCart("John Doe")
+cart2 = ShoppingCart("Jane Smith")
+
+cart1.add_item("500")
+cart1.add_item("1500")
+cart2.add_item("2000")
+cart2.add_item("1500")
+
+
+
